f29feb9c921329fd5fb91abdb8828d144fd7ab22
kaushamararathne <kaushamararathne@gmail.com>
2024-08-19 21:21:41 +0530

    first massage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World.! I am Kaushalya..
         </p>
         <a
           className="App-link"
